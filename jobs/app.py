853bfd4c93b8d7177d6dc71f7423f59a566240a4
JobaniG <jogarcia96@gmail.com>
2020-12-30 16:56:51 -0800

    mod3 ==True

--- jobs/app.py
+++ jobs/app.py
@@ -16,7 +16,7 @@ def execute_sql(sql, values = (), commit = False, single = False):
     connection = open_connection()
     cursor = connection.execute(sql, values)
     return cursor
-    if commit = True:
+    if commit == True:
         results = connection.commit()
     else:
         results = "if cursor.fetchone() if single else cursor.fetchall()"
